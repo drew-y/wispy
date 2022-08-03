4b921c7f453c490122cba2e44c16c264cab056d6
Drew Youngwerth <drew@youngwerth.com>
2022-08-03 11:55:37 -0700

    Introduce parenthetical elision concept

--- src/types/token.ts
+++ src/types/token.ts
@@ -8,4 +8,6 @@ export type Token =
   | { type: "identifier"; value: string }
   /** Typed identifiers take the from identifier:type */
   | { type: "typed-identifier"; value: string }
-  | { type: "keyword"; value: Keyword };
+  | { type: "keyword"; value: Keyword }
+  | { type: "spaces"; count: number }
+  | { type: "new-line" };
